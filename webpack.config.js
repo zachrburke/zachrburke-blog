554fb0289c7e1010e569c518462e12015fe271de
Zach <madutilitybelt@gmail.com>
2017-06-04 21:43:14 -0500

    adding .nvmrc to try and fix netlify
    also using opan sans, and fixing some pathing now that I'm not using azure

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,23 @@ module.exports = {
   devServer: {
     https: true,
     historyApiFallback: true,
+    publicPath: "/dist/"
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
       }
     ]
   }
