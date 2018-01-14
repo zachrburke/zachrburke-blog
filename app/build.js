18c47d0d7b11a11e1764d58f25269b5d0f603831
Zach <madutilitybelt@gmail.com>
2018-01-14 17:26:03 -0600

    fixed syntax error for lower node version running on netlify

--- app/build.js
+++ app/build.js
@@ -19,25 +19,25 @@ const getPostWithBody = (post) => {
     const markdown = fse.readFileSync('./content/posts/' + post.filename).toString();
     const converter = new showdown.Converter();
 
-    return {
-        ...post,
+    return Object.assign({}, post, {
         body: converter.makeHtml(markdown),
         filename: post.filename.replace('.md', '.html'),
-    }
+    });
 }
 
 applyTo('main section.archive')(templates.archive(posts));
 
-if (fse.existsSync('./dist/')) fse.removeSync('./dist/');
-fse.mkdirSync('./dist/');
-fse.mkdirSync('./dist/blog/');
+if (!fse.existsSync('./dist/')) {
+    fse.mkdirSync('./dist/');
+    fse.mkdirSync('./dist/blog/');
+    fse.mkdirSync('./dist/static');
+}
 
 var latestPost = getPostWithBody(posts[0]);
 console.log('Creating home page using', latestPost.filename);
 
 applyTo('main section.post-content')(templates.post(latestPost));
 fse.writeFileSync('./dist/index.html', $.html());
-
 
 posts.forEach(post => {
     const postWithBody = getPostWithBody(post);
@@ -49,9 +49,7 @@ posts.forEach(post => {
 
 fse.copySync('./content/', './dist/content');
 
-fse.mkdirSync('./dist/static');
 const css = fse.readFileSync('./app/index.css').toString();
-
 postcss([ autoprefixer ]).process(css).then(function (result) {
     result.warnings().forEach(function (warn) {
         console.warn(warn.toString());
